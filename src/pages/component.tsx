1c6a07fc0df429977af8ae5ee636a6fdecbe5338
bn-bushra <aybuhabi@gmail.com>
2024-03-14 12:48:02 +0100

    fix: refactor

--- src/pages/component.tsx
+++ src/pages/component.tsx
@@ -2,6 +2,7 @@ import Pc from "../assets/newComp/pc.svg";
 import MockupHand from "../assets/newComp/logo mockup.png";
 import Dollar from "../assets/newComp/dollar.svg";
 import DonateImage from "../assets/newComp/donate.svg";
+import { url } from "inspector";
 
 type SatifactoryProps = {
   title: string;
@@ -20,7 +21,7 @@ const Satifactory: React.FC<SatifactoryProps> = ({ title, tag, paragraph }) => {
           <h3 className="capitalise leading-[62.4px] mt-[19.04px] font-black font-Satoshi text-[51.2px] md:text-[56px] text-white">
             {title}
           </h3>
-          <p className="font-Inter text-white leading-[25.6px] md:text-lg font-normal">
+          <p className=" space-y-6 mt-7 mb-11 font-Inter text-white leading-[2rem] md:text-lg font-normal">
             {paragraph}
           </p>
         </div>
@@ -42,18 +43,23 @@ const Building: React.FC<BuildingProps> = ({ title, content }) => {
           <h3 className="capitalise leading-[62.4px] font-black font-Satoshi text-[51.2px] md:text-[56px] text-white">
             {title}
           </h3>
-          <div className="flex flex-col gap-8 justify-center mt-5">
+          <div className="flex flex-col gap-8 justify-center mt-14">
             {content.map((item, index) => (
               <div
                 key={index}
-                className="flex gap-4 items-center font-Inter text-white leading-[25.6px] text-lg font-normal mt-14"
+                className="flex gap-4 items-center font-Inter text-white leading-[25.6px] text-lg font-normal "
               >
-                <img src={item.image} alt="" />
+                <img src={item.image} alt="" className="size-[38px]" />
                 <p>{item.text}</p>
               </div>
             ))}
           </div>
         </div>
+        <img
+          src={MockupHand}
+          alt="Emerge Mockup"
+          className="mx-auto hidden lg:block  scale-90 lg:absolute lg:right-36 lg:bottom-[-48px] lg:scale-150"
+        />
       </div>
     </section>
   );
@@ -68,7 +74,7 @@ type BookDemoProps = {
   };
   media: {
     isImage: boolean;
-    url?: string;
+    url: string;
   };
 };
 
@@ -107,6 +113,64 @@ function BookDemo({ content, media }: BookDemoProps) {
   );
 }
 
+type BookDemo1Props = {
+  content: {
+    tag?: string;
+    title: string;
+    paragraph: string;
+  };
+  media: {
+    url: string;
+    alt?: string;
+  };
+  isImage?: boolean;
+};
+
+function BookDemo1({ content, media, isImage = false }: BookDemo1Props) {
+  return (
+    <section className="bg-[#100650] w-full flex bg-[url('../public/art.svg')]  py-[12.5rem] px-[32.96px] md:px-32 pt-[56px] pb-[352px] md:py-40 bg-origin-border bg-no-repeat bg-center md:bg-right-top min-h-screen">
+      <div className="w-full flex flex-col lg:flex-row gap-10 items-center justify-center md:gap-8 md:justify-between">
+        <div className="flex flex-col items-start gap-4 w-full ">
+          <span className=" text-sm md:text-base text-center font-Inter  text-white font-medium uppercase h-[2.25rem] bg-white/10 px-2.5 rounded-[.375rem] py-2">
+            {content.tag}
+          </span>
+          <div className="flex flex-col items-start gap-9 text-white">
+            <h3 className="capitalise leading-[62.4px] font-black font-Satoshi text-[51.2px] md:text-[56px]">
+              {content.title}
+            </h3>
+            <p className="font-Inter text-white leading-[25.6px] md:text-lg font-normal">
+              {content.paragraph}
+            </p>
+
+            <button className="bg-[#8959FF] px-[1.875rem] py-3 rounded-lg font-semibold text-lg">
+              Book a Demo
+            </button>
+          </div>
+        </div>
+        <div className="w-full object-fill bg-[#D1D0D7] rounded-2xl shadow-md ">
+          {isImage ? (
+            <img width={545} height={365} src={media.url} alt={media.alt}></img>
+          ) : (
+            <video>
+              <source src={media.url} />
+            </video>
+          )}
+          <iframe
+            width="545"
+            height="365"
+            src="https://www.youtube.com/embed/dSufVTSCeVw"
+            title="Builder Studio"
+            loading="lazy"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            className="hidden w-full object-fill bg-[#D1D0D7] rounded-2xl shadow-md "
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  );
+}
+
 const NewComponent = () => {
   return (
     <>
@@ -136,41 +200,38 @@ const NewComponent = () => {
         ]}
       />
 
-      <section className="bg-[#100650] w-full overflow-hidden">
-        <div className="flex px-[32.96px] lg:px-32 pt-[56px] pb-[352px] md:py-40 flex-col lg:flex-row bg-[url(./assets/vector-mobile.svg)] lg:bg-[url('./assets/vector-ppc.svg')] bg-origin-border bg-no-repeat bg-right-top lg:bg-right-bottom relative">
-          <div className="w-full md:w-2/3 ">
-            <h3 className="capitalise leading-[62.4px] font-black font-Satoshi text-[51.2px] md:text-[56px] text-white">
-              Even if you don't know how to code, you can build your app idea.
-            </h3>
-            <div className="flex flex-col gap-8 justify-center mt-5">
-              <div className="flex gap-4 items-center font-Inter text-white leading-[25.6px] text-lg font-normal mt-14">
-                <img src={Pc} alt="" />
-                <p>
-                  Flexible - we can build anything regardless of the complexity
-                </p>
-              </div>
-              <div className="flex gap-4 items-center font-Inter text-white leading-[25.6px] text-lg font-normal">
-                <span className="w-[2.375rem] flex justify-center items-center">
-                  <img src={Dollar} alt="Dollar sign" />
-                </span>
-                <p>Guaranteed pricing and set timelines upfront using AI</p>
-              </div>
-              <div className="flex gap-4 items-center font-Inter text-white leading-[25.6px] text-lg font-normal">
-                <img src={DonateImage} alt="Donate Icon" />
-                <p>Aftercare - keeps your software up to date with no fuss</p>
-              </div>
-            </div>
-          </div>
-          <img
-            src={MockupHand}
-            alt="Mockup"
-            className="mx-auto hidden lg:block  scale-90 lg:absolute lg:right-36 lg:bottom-[-48px] lg:scale-150"
-          />
-        </div>
-      </section>
-      <section className="bg-[#100650] w-full flex bg-[url('../public/art.svg')]  py-[12.5rem] px-[32.96px] md:px-32 pt-[56px] pb-[352px] md:py-40 bg-origin-border bg-no-repeat bg-right-bottom md:bg-right-top h-screen">
-        <div className="w-full flex flex-col md:flex-row gap-8 items-center justify-center md:gap-0 md:justify-between flex-1">
-          <div className="flex flex-col items-start gap-4">
+      <BookDemo1
+        content={{
+          tag: "building",
+          title:
+            "Indutries transformed with the best mobile app development services",
+          paragraph:
+            "We are a top-rated app development company in Dubai, UAE, with a reputation for developing highly engaging mobile apps for a variety of business needs.",
+        }}
+        media={{
+          url: "https://source.unsplash.com/random",
+          alt: "Builder Studio",
+        }}
+        isImage={false}
+      />
+
+      <BookDemo
+        content={{
+          tag: "Building",
+          title:
+            "Indutries transformed with the best mobile app development services",
+          paragraph:
+            "We are a top-rated app development company in Dubai, UAE, with a reputation for developing highly engaging mobile apps for a variety of business needs.",
+          buttonLabel: "Book a Demo",
+        }} media={
+       {   isImage: false
+          url: {"https://source.unsplash.com/random"}
+       }}
+      />
+
+      <section className="hidden bg-[#100650] w-full flex bg-[url('../public/art.svg')]  py-[12.5rem] px-[32.96px] md:px-32 pt-[56px] pb-[352px] md:py-40 bg-origin-border bg-no-repeat bg-center md:bg-right-top min-h-screen">
+        <div className="w-full flex flex-col lg:flex-row gap-10 items-center justify-center md:gap-8 md:justify-between">
+          <div className="flex flex-col items-start gap-4 w-full ">
             <span className=" text-sm md:text-base text-center font-Inter  text-white font-medium uppercase h-[2.25rem] bg-white/10 px-2.5 rounded-[.375rem] py-2">
               BUILDING
             </span>
@@ -190,17 +251,16 @@ const NewComponent = () => {
               </button>
             </div>
           </div>
-          <div className="w-full">
+          <div className="w-full ">
             <iframe
               width="545"
               height="365"
               src="https://www.youtube.com/embed/dSufVTSCeVw"
               title="Builder Studio"
-              frameBorder="0"
               loading="lazy"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
-              className="bg-[#D1D0D7] rounded-3xl shadow-md shadow-"
+              className="w-full object-fill bg-[#D1D0D7] rounded-2xl shadow-md "
             ></iframe>
           </div>
         </div>
